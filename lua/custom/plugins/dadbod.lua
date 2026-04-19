3d46d3a888cdef1e7882d66134a6d36a4797621e
Vasily Kozhukhovskiy <kojuhovskiy@gmail.com>
2026-04-19 20:56:35 +0200

    turn off exec on save for dadbod ui

--- lua/custom/plugins/dadbod.lua
+++ lua/custom/plugins/dadbod.lua
@@ -6,6 +6,9 @@ return {
       'kristijanhusak/vim-dadbod-completion',
     },
     cmd = { 'DB', 'DBUI', 'DBUIToggle', 'DBUIAddConnection', 'DBUIFindBuffer' },
-    config = function() vim.g.db_ui_use_nerd_fonts = vim.g.have_nerd_font end,
+    config = function()
+      vim.g.db_ui_use_nerd_fonts = vim.g.have_nerd_font
+      vim.g.db_ui_execute_on_save = 0
+    end,
   },
 }
